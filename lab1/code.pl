05a052cc72bb5822f03fde8b2e96ce448b5fab40
Daniil <dr.chashka@gmail.com>
2024-11-12 18:10:59 +0300

    Сгрузил в первой лабе всё в один файл.

--- lab1/code.pl
+++ lab1/code.pl
@@ -34,3 +34,26 @@ aunt(X, Y) :- parent(P, Y), sister(X, P).
 
 % f) Определите отношение "кузин".
 cousin(X, Y) :- parent(PX, X), parent(PY, Y), parent(P, PX), parent(P, PY), different(PX, PY).
+
+likes(ellen, reading).
+likes(john, computers).
+likes(john, badminton).
+likes(john, photo).
+likes(john, reading).
+likes(leonard, badminton).
+likes(eric, swimming).
+likes(eric, badminton).
+likes(eric, chess).
+likes(eric, game).
+likes(paul, swimming).
+
+% b) Найти тех, кто имеет четыре хобби.
+has_four_hobbies(Person) :-
+    findall(Hobby, likes(Person, Hobby), Hobbies),
+    length(Hobbies, 4).
+
+% c) Найти тех, у кого одинаковые хобби.
+same_hobbies(Person1, Person2) :-
+    likes(Person1, Hobby),
+    likes(Person2, Hobby),
+    different(Person1, Person2).
