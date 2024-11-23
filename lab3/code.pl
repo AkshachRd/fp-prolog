c0306215988bcca52660d7d340ecd98f3b6eab9c
Daniil <dr.chashka@gmail.com>
2024-11-23 21:38:22 +0300

    Сделал 3 лабу.

---
+++ lab3/code.pl
@@ -0,0 +1,53 @@
+trim([_|Rest], L2) :-
+    append(L2, [_], Rest).
+
+first_last([First|Rest], [Last|NewRest]) :-
+    append(Middle, [Last], Rest),
+    append(Middle, [First], NewRest).
+
+total([], 1).
+total([X | Xs], N) :-
+    total(Xs, N1),
+    N is X * N1.
+
+place(X, List, [X | List]).
+place(X, [Y | Ys], [Y | Zs]) :-
+    place(X, Ys, Zs).
+
+double([], []).
+double([X | Xs], [X, X | Ys]) :-
+    double(Xs, Ys).
+
+split([], [], []).
+split([X | Xs], Evens, Odds) :-
+    split(Xs, Evens1, Odds1),
+    ( 0 is X mod 2 ->
+        Evens = [Evens1 | X],
+        Odds = Odds1
+    ;
+        Evens = Evens1,
+        Odds = [Odds1 | X]
+    ).
+
+repeat3(L1, L2) :-
+    swap_first_last(L1, SwappedL1),
+    SwappedL1 = [First | Middle],
+    last(SwappedL1, Last),
+    replicate(First, 3, FirstList),
+    replicate(Last, 3, LastList),
+    append([FirstList, Middle, LastList], L2).
+
+swap_first_last([First | Rest], [Last | NewRest]) :-
+    append(Middle, [Last], Rest),
+    append(Middle, [First], NewRest).
+
+replicate(_, 0, []).
+replicate(X, N, [X | Xs]) :-
+    N > 0,
+    N1 is N - 1,
+    replicate(X, N1, Xs).
+
+combi([X], _, [X]).
+combi([X1, X2 | Xs], [Y | Ys], [X1, Y | Zs]) :-
+    combi([X2 | Xs], Ys, Zs).
+combi([X1, X2 | Xs], [], [X1, X2 | Xs]).
